663ba5533765203b19448946a8ee9bdcab3704c1
Sergei-Shmakov <153823538+Sergei-Shmakov@users.noreply.github.com>
2023-12-28 19:11:39 +0700

    Update final_win.py

--- final_win.py
+++ final_win.py
@@ -1,17 +1,19 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit,  QHBoxLayout
-from instr.py import *
+from instr import *
+from second_win import *
 
-class FinalWim(QWidget):
-    def __init__(self):
+class FinalWin(QWidget):
+    def __init__(self, exp):
         super().__init__()
+        self.exp = exp
         self.initUI()
         self.set_appear()
         self.show()
 
     def initUI(self):
-        self.workh_text = QLabel(txt_workheart)
-        self.index_text = QLabel(txt_index)
+        self.workh_text = QLabel(txt_workheart + self.results())
+        self.index_text = QLabel(txt_index + str(self.index))
 
         self.layout_line = QVBoxLayout()
         self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
@@ -20,5 +22,66 @@ class FinalWim(QWidget):
 
     def set_appear(self):
         self.setWindowTitle(txt_finalwin)
-        self.resize(win_widht, win_height)
-        self.move(win_x, win-y)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def results(self):
+        if self.exp.age < 7:
+            self.index = 0
+            return 'нет данных для такого возраста'
+        self.index = (4 * (int(self.exp.t1) + int(self.exp.t2) + int(self.exp.t3)) - 200) / 10
+        if self.exp.age == 7 or self.exp.age == 8:
+            if self.index >= 21:
+                return txt_res1
+            elif self.index >= 17 and self.index <= 20.9:
+                return txt_res2
+            elif self.index >= 12 and self.index <= 16.9:
+                return txt_res3
+            elif self.index >= 6.5 and self.index <= 11.9:
+                return txt_res4
+            elif self.index <= 6.4:
+                return txt_res5
+        elif self.exp.age == 9 or self.exp.age == 10:
+            if self.index >= 19.5:
+                return txt_res1
+            elif self.index >= 15.5 and self.index <= 19.4:
+                return txt_res2
+            elif self.index >= 10.5 and self.index <= 15.4:
+                return txt_res3
+            elif self.index >= 5 and self.index <= 10.4:
+                return txt_res4
+            elif self.index <= 4.9:
+                return txt_res5
+        elif self.exp.age == 11 or self.exp.age == 12:
+            if self.index >= 18:
+                return txt_res1
+            elif self.index >= 14 and self.index <= 17.9:
+                return txt_res2
+            elif self.index >= 9 and self.index <= 13.9:
+                return txt_res3
+            elif self.index >= 3.5 and self.index <= 8.9:
+                return txt_res4
+            elif self.index <= 3.4:
+                return txt_res5
+        elif self.exp.age == 13 or self.exp.age == 14:
+            if self.index >= 16.5:
+                return txt_res1
+            elif self.index >= 12.5 and self.index <= 16.4:
+                return txt_res2
+            elif self.index >= 7.5 and self.index <= 12.4:
+                return txt_res3
+            elif self.index >= 2 and self.index <= 7.4:
+                return txt_res4
+            elif self.index <= 1.9:
+                return txt_res5
+        elif self.exp.age >= 15:
+            if self.index >= 15:
+                return txt_res1
+            elif self.index >= 11 and self.index <= 14.9:
+                return txt_res2
+            elif self.index >= 6 and self.index <= 10.9:
+                return txt_res3
+            elif self.index >= 0.5 and self.index <= 5.9:
+                return txt_res4
+            elif self.index <= 0.4:
+                return txt_res5
